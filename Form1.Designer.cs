7d21adc69082532c36cea340d4d8cc6359b1212e
Logan Kim <logeonwoo05@gmail.com>
2026-04-09 15:37:56 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace BurgerKiosk
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            listBox1 = new ListBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            checkBox4 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
+            groupBox3 = new GroupBox();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 25F, FontStyle.Bold);
+            label1.ForeColor = SystemColors.Highlight;
+            label1.Location = new Point(142, 104);
+            label1.Name = "label1";
+            label1.Size = new Size(622, 89);
+            label1.TabIndex = 0;
+            label1.Text = "버거 주문 키오스크";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 12F);
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(78, 419);
+            label2.Name = "label2";
+            label2.Size = new Size(206, 45);
+            label2.TabIndex = 2;
+            label2.Text = "총 금액 : 0원";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1337, 768);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 46);
+            button1.TabIndex = 3;
+            button1.Text = "주문하기";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(1517, 768);
+            button2.Name = "button2";
+            button2.Size = new Size(150, 46);
+            button2.TabIndex = 4;
+            button2.Text = "초기화";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(28, 54);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(378, 328);
+            listBox1.TabIndex = 5;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.ForeColor = Color.Black;
+            radioButton1.Location = new Point(27, 86);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(174, 58);
+            radioButton1.TabIndex = 6;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "햄버거";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.ForeColor = Color.Black;
+            radioButton2.Location = new Point(27, 265);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(254, 58);
+            radioButton2.TabIndex = 7;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "불고기버거";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.ForeColor = Color.Black;
+            radioButton3.Location = new Point(27, 433);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(214, 58);
+            radioButton3.TabIndex = 8;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "치킨버거";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(pictureBox3);
+            groupBox1.Controls.Add(pictureBox2);
+            groupBox1.Controls.Add(pictureBox1);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Font = new Font("맑은 고딕", 15F);
+            groupBox1.ForeColor = Color.Red;
+            groupBox1.Location = new Point(115, 246);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(564, 568);
+            groupBox1.TabIndex = 9;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "메뉴 선택";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(checkBox4);
+            groupBox2.Controls.Add(checkBox3);
+            groupBox2.Controls.Add(checkBox2);
+            groupBox2.Controls.Add(checkBox1);
+            groupBox2.Font = new Font("맑은 고딕", 15F);
+            groupBox2.ForeColor = Color.Red;
+            groupBox2.Location = new Point(789, 246);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(400, 426);
+            groupBox2.TabIndex = 9;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "추가 옵션";
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.ForeColor = Color.Black;
+            checkBox4.Location = new Point(21, 358);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(229, 58);
+            checkBox4.TabIndex = 3;
+            checkBox4.Text = "소스 추가";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.ForeColor = Color.Black;
+            checkBox3.Location = new Point(21, 254);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(229, 58);
+            checkBox3.TabIndex = 2;
+            checkBox3.Text = "치즈 추가";
+            checkBox3.UseVisualStyleBackColor = true;
+            checkBox3.CheckedChanged += checkBox3_CheckedChanged;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.ForeColor = Color.Black;
+            checkBox2.Location = new Point(21, 152);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(135, 58);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "콜라";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.ForeColor = Color.Black;
+            checkBox1.Location = new Point(21, 60);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(215, 58);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "감자튀김";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(listBox1);
+            groupBox3.Controls.Add(label2);
+            groupBox3.Font = new Font("맑은 고딕", 15F);
+            groupBox3.ForeColor = Color.Red;
+            groupBox3.Location = new Point(1309, 246);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(454, 478);
+            groupBox3.TabIndex = 9;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "주문 내역";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(330, 54);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(210, 146);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(300, 238);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(264, 202);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 11;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(316, 419);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(190, 156);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 12;
+            pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1816, 858);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Burger Kiosk v1.0";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private ListBox listBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private CheckBox checkBox4;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private GroupBox groupBox3;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace BurgerKiosk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
